8f42767e94d21f81e1343d222c7936ef79111cd7
trpowerr <mixazbubl@gmail.com>
2019-12-18 13:27:31 +0300

    first commit

---
+++ src/js/class.js
@@ -0,0 +1,63 @@
+class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+    }
+  }
+  
+  class Bowman extends Character {
+    constructor(name) {
+      super(name);
+      this.attack = 25;
+      this.defence = 25;
+    }
+  }
+  
+  class Swordman extends Character {
+    constructor(name) {
+      super(name);
+      this.attack = 40;
+      this.defence = 10;
+    }
+  }
+  
+  class Magician extends Character {
+    constructor(name) {
+      super(name);
+      this.attack = 10;
+      this.defence = 40;
+    }
+  }
+  
+  class Undead extends Character {
+    constructor(name) {
+      super(name);
+      this.attack = 25;
+      this.defence = 25;
+    }
+  }
+  
+  class Zombie extends Character {
+    constructor(name) {
+      super(name);
+      this.attack = 40;
+      this.defence = 10;
+    }
+  }
+  
+  class Demon extends Character {
+    constructor(name) {
+      super(name);
+      this.attack = 10;
+      this.defence = 40;
+    }
+  }
+  
+  
+  
+  
+  const newPersonTwo = new Zombie ('Misha');
+  
+  console.log(newPersonTwo);
+  
+  
